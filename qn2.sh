be377dd547bce5754de2397ccc87cf8d42507dbb
MahaLakshmi-2002 <87593232+MahaLakshmi-2002@users.noreply.github.com>
2023-08-03 15:17:32 +0530

    Create qn2.sh
    Write a script that takes a filename as input and creates a backup of that file by adding a timestamp to its name.

---
+++ qn2.sh
@@ -0,0 +1,11 @@
+#Write a script that takes a filename as input and creates a backup of that file by adding a timestamp to its name.
+#!/bin/bash
+echo "enter a file name"
+read file
+filename=$file
+currenttime=$(date +"%Y.%m.%d-%H.%M.%S")
+echo "currentime is $currenttime"
+newfile=$filename.$currenttime
+echo "newfile=$newfile"
+backup=$newfile
+cp $filename $backup
